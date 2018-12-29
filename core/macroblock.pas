8c794e5f60027b48844f2cecc32977714f19551e
dpethes <dejvu@email.cz>
2018-12-29 10:50:47 +0100

    raise chroma residual coding treshold
    Allows more PSkips in areas where P16x16 with only small chroma residual would be used.

--- core/macroblock.pas
+++ core/macroblock.pas
@@ -204,19 +204,24 @@ begin
 
   //intra cache
   fill_intra_pred_cache(mb, frame);
+
+  //debug: clear some data areas that don't need clearing under normal circumstances
+  {
+  FillByte(mb.block, SizeOf(mb.block), 0);
+  }
 end;
 
 
 (*******************************************************************************
 intra coding
 *)
-const SAD_DECIMATE_TRESH: array[0..51] of word = (
+const SAD_DECIMATE_TRESH: array[0..54] of word = (
     3,   3,   3,   3,   3,   3,   4,   4,   5,   5,
     6,   7,   8,   9,  10,  11,  13,  14,  16,  18,
    20,  22,  26,  28,  32,  36,  40,  44,  52,  56,
    64,  72,  80,  88, 104, 112, 128, 144, 160, 176,
   208, 224, 256, 288, 320, 352, 416, 448, 512, 576,
-  640, 704
+  640, 704, 800, 900, 1000  //chroma uses qp+3
 );
 
 procedure block_use_zero(var b: block_t);
@@ -410,7 +415,7 @@ begin
      mb.chroma_dc[0, i] := 0;
      mb.chroma_dc[1, i] := 0;
   end;
-  sad_tresh := SAD_DECIMATE_TRESH[mb.qpc];
+  sad_tresh := SAD_DECIMATE_TRESH[mb.qpc+3];
 
   if intra then
       intrapred.Analyse_8x8_chroma(mb.pfdec_c[0], mb.pfdec_c[1], mb.chroma_pred_mode);
